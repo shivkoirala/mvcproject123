3846839293d95e550c3423c2f98157c63882a267
opensource-emr <48054642+opensource-emr@users.noreply.github.com>
2023-01-14 19:42:39 +0530

    committ

--- WebApplication2/WebApplication2/obj/Debug/netcoreapp3.1/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/WebApplication2/obj/Debug/netcoreapp3.1/WebApplication2.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("f3f5641e-d896-44ac-b442-808926a9df7b")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
